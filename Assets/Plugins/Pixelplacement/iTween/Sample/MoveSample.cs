eaf38ac9d2f3324ea7d382062a52b4c996330d1e
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-12-15 00:47:27 +0900

    配信時の編集分（あまり意味ない）

--- Assets/Plugins/Pixelplacement/iTween/Sample/MoveSample.cs
+++ Assets/Plugins/Pixelplacement/iTween/Sample/MoveSample.cs
@@ -2,9 +2,53 @@ using UnityEngine;
 using System.Collections;
 
 public class MoveSample : MonoBehaviour
-{	
+{
+	public float m_fSafeTime;
+	public void MapStart()
+	{
+		if( 0 < m_fSafeTime)
+		{
+			// –³“Gˆ—ÄŠJ
+			StartCoroutine(SafeAction(m_fSafeTime));
+		}
+	}
+	public IEnumerator SafeAction(float _fTime)
+	{
+		// –³“Gˆ—ŠJŽn
+		while(0 < m_fSafeTime)
+		{
+			m_fSafeTime -= Time.deltaTime;
+			yield return null;
+		}
+		//–³“G‰ðœ
+	}
+
+	public void Damage()
+	{
+		if (0 < m_fSafeTime)
+		{
+			// –³“GŽžŠÔ‚È‚Ì‚Åƒ_ƒ[ƒW‚ðŽó‚¯‚È‚¢
+		}
+		else
+		{
+			m_fSafeTime += 3.0f;
+		}
+	}
+	private void Update()
+	{
+		if (0 < m_fSafeTime)
+		{
+			m_fSafeTime -= Time.deltaTime;
+		}
+	}
+
+
 	void Start(){
 		iTween.MoveBy(gameObject, iTween.Hash("x", 2, "easeType", "easeInOutExpo", "loopType", "pingPong", "delay", .1));
 	}
+
+
+
+
 }
 
