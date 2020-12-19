aeb880b91a36c3fe4ab8749d61c97ebc5086a9d9
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-12-19 13:31:58 +0900

    画像移動

---
+++ Assets/Scripts/Data/DataItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable, CreateAssetMenu(fileName = "DataItem", menuName = "ScriptableObject/DataItem")]
+public class DataItem : ScriptableObject
+{
+	public string item_name;
+	public string item_key;
+
+	public Sprite sprite;
+}
